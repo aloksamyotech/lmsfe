caffdbbc2d5e0bd9ae7892201002c932474f0254
diksha <diksha.kushwah@samyotech.in>
2025-04-24 18:58:23 +0530

    remove

--- src/views/EmailTemplates/index.js
+++ src/views/EmailTemplates/index.js
@@ -89,7 +89,6 @@ const EmailTemplates = () => {
         getApi(`${url.purchaseBook.purchaseReport}${startDate}/${endDate}`),
         getApi(`${url.booksubmission.getsubmitedBook}`)
       ]);
-      console.log("submissionResult",submissionResult);
       
       if (bookAllotmentResult.status === 'fulfilled') {
         const responseData = bookAllotmentResult.value?.data;
