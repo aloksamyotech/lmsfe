7ca9c745371f548311f03fa71f54adba4ab346f5
diksha <diksha.kushwah@samyotech.in>
2025-05-06 20:41:07 +0530

    remove

--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -1,17 +1,14 @@
 import { useEffect, useState } from 'react';
 
-// material-ui
 import { Grid } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
-// project imports
-// import EarningCard from '../EarningCard';
+
 import EarningCard from './EarningCard';
 import PopularCard from './PopularCard';
 
 import TotalOrderLineChartCard from './TotalOrderLineChartCard';
-// import TotalIncomeDarkCard from './TotalIncomeDarkCard';
-//import TotalIncomeLightCard from './TotalIncomeLightCard';
+
 import TotalGrowthBarChart from './TotalGrowthBarChart';
 import { gridSpacing } from 'store/constant';
 import AppTrafficBySite from './TrafficBySiteCard';
@@ -20,7 +17,6 @@ import AppTasks from './AppTask';
 import AppConversionRates from './AppConversionCard';
 import AppCurrentVisits from './AppCurrentVisitCard';
 import TotalOrderLineChartCardd from './TotalOrderLineChaetCardd';
-// import EarningCardd from './EarningCardd';
 import EarningCardd from './EarningCardd';
 import BookIssued from './BookIssued';
 import ListedCategories from './ListedCategories';
@@ -55,18 +51,6 @@ const Dashboard = () => {
           </Grid>
         </Grid>
       </Grid>
-      {/* <Grid item xs={12}>
-        <Grid container spacing={gridSpacing}>
-          <Grid item xs={12} md={12}>
-            <TrandingBook isLoading={isLoading} />
-          </Grid>
-        </Grid>
-      </Grid>*/}
-      {/* <Grid item xs={12}>
-        <Grid item xs={12} md={12}>
-          <App isLoading={isLoading} />
-        </Grid>
-      </Grid>  */}
       <Grid item xs={12}>
         <Grid container spacing={gridSpacing}>
           <Grid item xs={12} md={12}>
