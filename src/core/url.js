87abc25521f571eadabd73180316da7c2ef0dea7
Diksha <diksha.kushwah@samyotech.in>
2025-06-18 15:58:34 +0530

    url fixs

--- src/core/url.js
+++ src/core/url.js
@@ -1,5 +1,6 @@
-const base_url = process.env.REACT_APP_BASE_URL;
+// const base_url = process.env.REACT_APP_BASE_URL;
 // const base_url = 'http://localhost:4300/';
+const base_url = 'https://smartlib.samyotech.in/api/';
 
 export const url = {
   baseurl: {
